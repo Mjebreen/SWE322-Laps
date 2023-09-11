6ea6dd315a368d797c5ac5c5baeb86e74c8e4e4e
Mjebreen <105075943+Mjebreen@users.noreply.github.com>
2023-09-11 15:37:19 +0300

    Add files via upload

---
+++ Lap2/Cal.php
@@ -0,0 +1,34 @@
+
+<?php
+
+
+echo "<h1>Mohammed Jebreen 202111314</h1>";
+
+
+
+function add($x, $y) {
+    return $x + $y;
+}
+
+function subtract($x, $y) {
+    return $x - $y;
+}
+
+function multiply($x, $y) {
+    return $x * $y;
+}
+
+function divide($x, $y) {
+    return $x / $y;;
+    }
+ 
+
+    echo "5 * 4 = " . multiply(5,4,) . "<br>" ;
+    echo "5 + 4 = " . add(5,4,) . "<br>" ;
+    echo "5 - 4 = " . subtract(5,4,) . "<br>" ;
+    echo "5 / 4 = " . divide(5,4,) . "<br>" ;
+
+
+?>
+
+
